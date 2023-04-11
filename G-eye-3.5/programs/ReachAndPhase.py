22e79c3b3bc4554c3bdfb7a3112ad9d4f4dec67e
D-matsushita <kotakotadata0320@gmail.com>
2023-04-11 21:21:55 +0900

    GUI選択完了

--- G-eye-3.5/programs/ReachAndPhase.py
+++ G-eye-3.5/programs/ReachAndPhase.py
@@ -3,8 +3,10 @@ from defs import phase
 import math
 
 def RAP_def(pole_top,Y,h,w):
-    theta_Y=phase.HighToTheta(pole_top[1].astype(int),int(h/2),0.674)
-    theta_X=phase.HighToTheta(pole_top[0].astype(int),int(w/2),0.674*(w/h))
+    Y_max=0.674
+    X_max=(Y_max*w)/h
+    theta_Y=phase.HighToTheta(pole_top[1].astype(int),int(h/2),Y_max)
+    theta_X=phase.HighToTheta(pole_top[0].astype(int),int(w/2),X_max)
 
     Reach=Y/(math.cos(theta_X)*math.tan(theta_Y))
 
--- G-eye-3.5/test.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-
-img=np.ones((720,1280,3),dtype=np.uint8)*128
-
-img=np.array(img)
-img2=cv2.rectangle(img,(600,320),(680,400),(255,255,255),2)
-
-cv2.imwrite("./test.png",img2)
